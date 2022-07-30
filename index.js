643fcb6051888eab6878d32d18c570a28a569867
Olivier Paspuel <olivier_paspuel@hotmail.com>
2022-07-30 16:23:12 -0500

    exercises 1, 2, 3 completed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const { request } = require("http");
 const app = express();
@@ -5,26 +6,46 @@ const app = express();
 let notes = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
   },
 ];
 
 app.get("/", (request, response) => {
-  response.send("<h1>Hola mundo</h1>");
+  response.send("<h1>Hola mundo, si esta funcionando?</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/info", (request, response) => {
+  const amountOfPeople = notes.length;
+  const date = new Date();
+
+  response.send(
+    `<p>Phonebook has info for ${amountOfPeople} people</p><p>${date}</p>`
+  );
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  response.json(notes.find((note) => note.id === id));
 });
 
 const PORT = 3001;
